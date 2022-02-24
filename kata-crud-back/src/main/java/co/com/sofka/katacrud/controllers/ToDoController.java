bbba18832ac0ece0774d9218ad89e0d06b3c3a67
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-24 03:40:28 -0500

    CrossOrigin

--- kata-crud-back/src/main/java/co/com/sofka/katacrud/controllers/ToDoController.java
+++ kata-crud-back/src/main/java/co/com/sofka/katacrud/controllers/ToDoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.websocket.server.PathParam;
 
 @RestController
+@CrossOrigin (origins = "http://localhost:3000/")
 public class ToDoController {
 
     @Autowired
